f64576b0c075757334438a5a0b51ef3b6a746fb0
Santiago G. Princ <sgprinc@users.noreply.github.com>
2022-07-25 12:29:37 +0200

    General cleanup
    Reorganized some functions, removed unused controls, updated resources and tuned parameters.

---
+++ Licorice Hunter/README.md
@@ -0,0 +1,7 @@
+# Licorice Hunter
+
+In Licorice Hunter, you are a finnball who's scavenging for licorice candy in the snow.
+Although you are quite impervious to the cold, you are not inmune! Try to grab as much candy as you can while warming up in the saunas every now and then.
+
+# Controls
+You can control the finnball with the keyboard arrows.
--- Licorice Hunter/licorice-hunter.js
+++ Licorice Hunter/licorice-hunter.js
@@ -1,12 +1,15 @@
-// Create a canvas and append it to the html
+// Find the anchor for the game and initialize the canvas
+var anchor = document.getElementById("canvas-anchor");
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 
-// Set resolutions for the game canvas (can be made dynamic for the feature we discussed)
+// Set resolutions for the game canvas as multiples of the tile sizes
 canvas.id = "gameCanvas";
-canvas.width = 500;
-canvas.height = 500;
-document.body.insertBefore(canvas, document.body.firstChild);
+canvas.width = 50 * 10;
+canvas.height = 50 * 10;
+
+// Append the canvas to the element used as anchor
+anchor.appendChild(canvas, anchor);
 
 
 //=====================================
@@ -48,15 +51,15 @@ saunaImage.onload = function () {
 var finn = {
     x: null,
     y: null,
-    speed: 500 // (pixels/second)
+    speed: 300 // (pixels/second)
 };
 var licorice = {
     x: null,
-    y: null,
+    y: null
 };
 var sauna = {
     x: null,
-    y: null,
+    y: null
 };
 var licoricesFound = 0;
 var placeSauna = false;
@@ -68,13 +71,15 @@ var placeSauna = false;
 var keysDown = {}; // List to track the keys down (can be many)
 
 // Events for key-press and key-release
-addEventListener("keydown", function (key) {
-    key.preventDefault();
-    keysDown[key.keyCode] = true;
+addEventListener("keydown", function (event) {
+    event.preventDefault();
+    var key = event.key || event.keyCode;
+    keysDown[key] = true;
 }, false);
-addEventListener("keyup", function (key) {
-    key.preventDefault();
-    delete keysDown[key.keyCode];
+addEventListener("keyup", function (event) {
+    event.preventDefault();
+    var key = event.key || event.keyCode;
+    delete keysDown[key];
 }, false);
 
 
@@ -93,23 +98,67 @@ var reset = function () {
 };
 
 // Spawn a licorice somewhere in the map
-var spawnLicorice = function(){
+var spawnLicorice = function () {
     licorice.x = (Math.random() * (canvas.width - licoriceImage.width));
     licorice.y = (Math.random() * (canvas.height - licoriceImage.height));
 }
 
+// Spawn a sauna somewhere in the map
+var spawnSauna = function (place) {
+    // If the time is correct spawn in map
+    if (place) {
+        sauna.x = (Math.random() * (canvas.width - saunaImage.width));
+        sauna.y = (Math.random() * (canvas.height - saunaImage.height));
+
+        // If the time is not correct, spawn out of bounds
+    } else {
+        sauna.x = 500;
+        sauna.y = 500;
+    }
+}
+
+var checkCollisions = function () {
+    function areOverlapping(objectA, objectB) {
+        const tileSize = 50;
+
+        if (objectA.x < objectB.x + tileSize &&
+            objectA.x + tileSize > objectB.x &&
+            objectA.y < objectB.y + tileSize &&
+            tileSize + objectA.y > objectB.y) {
+            return true;
+        }
+        return false;
+    }
+    // Check if player and licorice collided
+    if (areOverlapping(finn, licorice)) {
+        console.log("Found licorice");
+        ++licoricesFound;
+        spawnLicorice();
+    }
+
+    // Check if player got into the sauna
+    if (areOverlapping(finn, sauna)) {
+        console.log("Warmed up at the sauna");
+        count += 5;
+        // Hide sauna
+        spawnSauna(false);
+    }
+}
+
+// =====================================
 // Update game objects - change player position based on key pressed
+// =====================================
 var moveFinn = function (modifier) {
-    if (38 in keysDown) { // Player is holding up key
+    if ("ArrowUp" in keysDown) { // Player is holding up key
         finn.y -= finn.speed * modifier;
     }
-    if (40 in keysDown) { // Player is holding down key
+    if ("ArrowDown" in keysDown) { // Player is holding down key
         finn.y += finn.speed * modifier;
     }
-    if (37 in keysDown) { // Player is holding left key
+    if ("ArrowLeft" in keysDown) { // Player is holding left key
         finn.x -= finn.speed * modifier;
     }
-    if (39 in keysDown) { // Player is holding right key
+    if ("ArrowRight" in keysDown) { // Player is holding right key
         finn.x += finn.speed * modifier;
     }
 
@@ -128,46 +177,10 @@ var moveFinn = function (modifier) {
     }
 };
 
-var checkCollision = function (){
-    // Check if player and licorice collider
-    var licCenterX = licorice.x + licoriceImage.width / 2;
-    var licCenterY = licorice.y + licoriceImage.height / 2;
 
-    if (licCenterX >= finn.x && licCenterX <= finn.x + finnImage.width
-        && licCenterY >= finn.y && licCenterY <= finn.y + finnImage.height) {
-        console.log("Found licorice");
-        ++licoricesFound;
-        spawnLicorice();
-    }
-
-    // Check if player got into the sauna
-    var saunaCenterX = sauna.x + saunaImage.width / 2;
-    var saunaCenterY = sauna.y + saunaImage.height / 2;
-    
-    if (saunaCenterX >= finn.x && saunaCenterX <= finn.x + finnImage.width
-        && saunaCenterY >= finn.y && saunaCenterY <= finn.y + finnImage.height) {
-        console.log("Warmed up at the sauna");
-        count += 5;
-        // Hide sauna
-        spawnSauna(false);
-    }
-}
-
-// Spawn a sauna somewhere in the map
-var spawnSauna = function(place){
-    // If the time is correct spawn in map
-    if (place) {
-        sauna.x = (Math.random() * (canvas.width - saunaImage.width));
-        sauna.y = (Math.random() * (canvas.height - saunaImage.height));
-
-    // If the time is not correct, spawn out of bounds
-    }else{
-        sauna.x = 500;
-        sauna.y = 500;
-    }
-}
-
-// Draw everything on the canvas
+// =====================================
+// Render functions
+// =====================================
 var render = function () {
     if (bgReady) {
         ctx.drawImage(bgImage, 0, 0);
@@ -182,44 +195,50 @@ var render = function () {
         ctx.drawImage(saunaImage, sauna.x, sauna.y);
     }
 
-    // Display score and time 
-    ctx.fillStyle = "rgb(100, 100, 100)";
-    ctx.font = "24px Helvetica";
-    ctx.textAlign = "left";
-    ctx.textBaseline = "top";
-    ctx.fillText("Licorices found: " + licoricesFound, 20, 20);
-    ctx.fillText("Time: " + count, 20, 50);
+    if (!finished) {
+        // Display score and time 
+        ctx.font = "22px Helvetica";
+        ctx.textAlign = "left";
+        ctx.textBaseline = "top";
+        ctx.fillStyle = "green";
+        ctx.fillText("Licorices found: " + licoricesFound, 20, 20);
+        ctx.fillStyle = "red";
+        ctx.fillText("Frezing in: " + count, 20, 50);
 
-    // Display game over message when timer finished
-    if (finished == true) {
-        ctx.fillText("You froze to death", canvas.width / 2 - 30, canvas.height / 2 + 10);
-        ctx.fillText("Score: " + licoricesFound, canvas.width / 2 - 30, canvas.height / 2 + 40);
+    } else {
+        // Display game over message when timer finished
+        ctx.font = "38px Helvetica";
+        ctx.textAlign = "center";
+        ctx.fillText("You froze!", canvas.width / 2, canvas.height / 2 - 40);
+        ctx.fillText("Score: " + licoricesFound, canvas.width / 2, canvas.height / 2 + 20);
         submitScore()
     }
 };
 
-// Messages to communicate with the GamePlatform
-function saveGame(){
+//========================================
+// Messages to communicate with a backend
+//========================================
+function saveGame() {
     let message = {
         messageType: "SAVE",
         gameState: {
             player: finn,
             score: licoricesFound,
             counter: counter,
-            finished : finished
+            finished: finished
         }
     };
     parent.postMessage(message);
 }
 
-function loadGame(){
+function loadGame() {
     let message = {
         messageType: "LOAD_REQUEST"
     };
     parent.postMessage(message);
 }
 
-function submitScore(){
+function submitScore() {
     let message = {
         messageType: "SCORE",
         score: licoricesFound
@@ -227,7 +246,7 @@ function submitScore(){
     parent.postMessage(message);
 }
 
-function settingMessage(){
+function settingMessage() {
     let message = {
         messageType: "SETTING",
         options: {
@@ -238,13 +257,13 @@ function settingMessage(){
     parent.postMessage(message);
 }
 
-function receiveMessage(event){
-    if(event.data.messageType == "LOAD"){
+function receiveMessage(event) {
+    if (event.data.messageType == "LOAD") {
         let data = event.data.gameState;
         finn = data.player;
         licoricesFound = data.score;
         counter = data.counter,
-        finished = data.finished;
+            finished = data.finished;
     }
 }
 
@@ -254,11 +273,11 @@ function receiveMessage(event){
 var count = 30; // how many seconds the game lasts for - default 30
 var finished = false;
 var counter = function () {
-    if (!finished){
+    if (!finished) {
         count = count - 1; // countown by 1 every second
-    
+
         // Every 7 seconds trigger the sauna random aparition
-        if (count % 7 == 0 || count % sauna == 6) {
+        if (count % 7 == 0) {
             spawnSauna(true);
         } else {
             spawnSauna(false);
@@ -277,14 +296,14 @@ var counter = function () {
         }
     }
 }
-// timer interval is every second (1000ms)
+// Timer interval is every second (1000ms)
 setInterval(counter, 1000);
 // The main game loop
 var main = function () {
     // Allow for movement
     moveFinn(0.02);
     // Check for collisions
-    checkCollision();
+    checkCollisions();
     // Render graphics
     render();
 
